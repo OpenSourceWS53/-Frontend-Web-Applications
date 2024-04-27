b499790881cc2d4d088f2553709087f856e6e4f8
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-04-27 03:36:04 -0500

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatButton, RouterLink, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend-web-applications';
+  options = [
+    { path: '/home', title: 'Home'},
+  ]
 }
---
+++ src/app/public/pages/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {AsyncPipe, NgForOf} from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [AsyncPipe,
+    MatGridListModule,
+    MatButtonModule,
+    RouterLink,
+    NgForOf],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  tiles = [
+    {img: 'assets/images/statical.png', text: 'Statical Reports', link: '/'},
+    {img: 'assets/images/registration.png', text: 'Crop Registration', link: '/'},
+    {img: 'assets/images/forum.png', text: 'Consultation Forum', link: '/forum'},
+    {img: 'assets/images/history.png', text: 'Crops History', link: '/'}
+  ];
+}
+
