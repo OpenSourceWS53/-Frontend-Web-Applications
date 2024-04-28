a770ea54a0e1e0bec14caed2d2c34e1ca25213e7
PaoloGuilen <u202124343@upc.edu.pe>
2024-04-28 18:39:43 -0500

    feat: added another pie chart which interacts with the db.json in addition to interacting with /controls section of it.

---
+++ src/app/crops/model/control-crop.entity.ts
@@ -0,0 +1,15 @@
+export class ControlCrop {
+id: number;
+date: string;
+cropId: number;
+leave: string;
+stem: string;
+soil: string;
+constructor(){
+  this.id = 0;
+  this.date = '';
+  this.cropId = 0;
+  this.leave = '';
+  this.stem = '';
+  this.soil = '';}
+}
--- src/app/crops/pages/crops-statistics/crops-statistics.component.ts
+++ src/app/crops/pages/crops-statistics/crops-statistics.component.ts
@@ -3,7 +3,9 @@ import { NgModule, Component, OnInit, ViewChild } from "@angular/core";
 import { NgApexchartsModule } from "ng-apexcharts";
 import { ChartComponent, ApexNonAxisChartSeries, ApexResponsive, ApexChart } from "ng-apexcharts";
 import { MatCardModule } from '@angular/material/card';
-import {CropsService} from "../../services/crops.service";
+import { CropsService } from "../../services/crops.service";
+import { MatDialog } from '@angular/material/dialog';
+import { ControlsToCropService } from "../../services/controls-to-crop.service";
 
 export type ChartOptions = {
   series: ApexNonAxisChartSeries;
@@ -20,13 +22,39 @@ export type ChartOptions = {
 export class CropsStatisticsComponent implements OnInit {
   @ViewChild("chart") chart!: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+  public controlChartOptions: Partial<ChartOptions>; // Nuevas opciones para el gráfico de controles
+  public mostRegisteredCrop: string = '';
+  public mostControlledCrop: string = '';
 
-  constructor(private cropService: CropsService) {
+  constructor(private cropService: CropsService, private controlsService: ControlsToCropService) { // Inyecta ControlsToCropService
     this.chartOptions = {
       series: [],
       chart: {
-        width: 600,
+        width: 500,
         type: "pie"
+      },
+      labels: [],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+
+
+    this.controlChartOptions = {
+      series: [],
+      chart: {
+        width: 500,
+        type: "pie" // Cambiado de "pie" a "bar"
       },
       labels: [],
       responsive: [
@@ -51,20 +79,42 @@ export class CropsStatisticsComponent implements OnInit {
         acc[crop.name] = (acc[crop.name] || 0) + 1;
         return acc;
       }, {});
+
       this.chartOptions.labels = Object.keys(counts);
       this.chartOptions.series = Object.values(counts);
+
+      let maxCount = Math.max(...this.chartOptions.series);
+      let index = this.chartOptions.series.indexOf(maxCount);
+      this.mostRegisteredCrop = this.chartOptions.labels[index];
+    });
+  };
+
+  private getAllControls() {
+    this.controlsService.getAll().subscribe((response: any) => {
+      const counts = response.reduce((acc: { [key: string]: number }, control: any) => {
+        acc[control.cropId] = (acc[control.cropId] || 0) + 1;
+        return acc;
+      }, {});
+
+      this.controlChartOptions.labels = Object.keys(counts);
+      this.controlChartOptions.series = Object.values(counts);
+
+      let maxCount = Math.max(...this.controlChartOptions.series);
+      let index = this.controlChartOptions.series.indexOf(maxCount);
+      this.mostControlledCrop = this.controlChartOptions.labels[index];
     });
   };
 
   ngOnInit() {
     this.getAllCrops();
+    this.getAllControls();
   }
 }
 
 @NgModule({
   declarations: [CropsStatisticsComponent],
   imports: [BrowserModule, NgApexchartsModule, MatCardModule],
-  providers: [CropsService],
+  providers: [CropsService, ControlsToCropService],
   bootstrap: [CropsStatisticsComponent]
 })
 export class AppModule { }
