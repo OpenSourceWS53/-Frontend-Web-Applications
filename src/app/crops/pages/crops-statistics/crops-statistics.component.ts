1cbf4c222209ff5b8b66e17f1dc2e5d8f7146a6e
PaoloGuilen <u202124343@upc.edu.pe>
2024-04-28 12:58:21 -0500

    feat: added pie chart to statistics section

--- src/app/crops/pages/crops-statistics/crops-statistics.component.ts
+++ src/app/crops/pages/crops-statistics/crops-statistics.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component, ViewChild } from "@angular/core";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { ChartComponent } from "ng-apexcharts";
+import { ApexNonAxisChartSeries, ApexResponsive, ApexChart } from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-crops-statistics',
-  standalone: true,
-  imports: [],
   templateUrl: './crops-statistics.component.html',
-  styleUrl: './crops-statistics.component.css'
+  styleUrls: ['./crops-statistics.component.css']
 })
 export class CropsStatisticsComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22], // Ensure series is always defined
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
 }
+
+@NgModule({
+  declarations: [CropsStatisticsComponent],
+  imports: [BrowserModule, NgApexchartsModule],
+  providers: [],
+  bootstrap: [CropsStatisticsComponent]
+})
+export class AppModule { }
