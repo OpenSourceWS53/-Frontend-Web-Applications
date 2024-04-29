fa49c12e837cdcaccde25cd66e565576cc3c937d
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-04-29 03:22:16 -0500

    feat general-information and crop-care slides

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {HomeComponent} from "./public/pages/home/home.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {CropsManagementComponent} from "./crops/pages/crops-management/crops-management.component";
 import {CropsHistoryComponent} from "./crops/pages/crops-history/crops-history.component";
+import {CropsInformationComponent} from "./crops/pages/crops-information/crops-information.component";
 /*import {CropsStatisticsComponent} from "./crops/pages/crops-statistics/crops-statistics.component";*/
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path:'mycrops', component: CropsManagementComponent},
     {path:'history', component: CropsHistoryComponent},
+    {path:'info', component: CropsInformationComponent},
     /*{path:'statistics', component: CropsStatisticsComponent},*/
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
---
+++ src/app/crops/model/care.entity.ts
@@ -0,0 +1,12 @@
+export class Care {
+id:number;
+cropId:number;
+date:string;
+suggestion:string;
+  constructor(){
+  this.id=0;
+  this.cropId=0;
+  this.date="";
+  this.suggestion="";
+  }
+}
