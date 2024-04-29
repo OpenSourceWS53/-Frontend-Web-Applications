fefc9237bc241e3dfcfe05a5ced2ca2156f73169
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-04-29 01:43:06 -0500

    feat crops-history section

--- src/app/crops/services/crops.service.ts
+++ src/app/crops/services/crops.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { BaseService } from "../../shared/services/base.service";
 import { HttpClient } from "@angular/common/http";
 import { Crop } from "../model/crop.entity";
-
 @Injectable({
   providedIn: 'root'
 })
