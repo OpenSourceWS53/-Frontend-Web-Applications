9c161314abc06e0ee6edcbb8796ea0bc287dd38b
dacantoralp <u20201f568@upc.edu.pe>
2024-04-30 09:29:02 -0500

    feat: added crop-information

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {HomeComponent} from "./public/pages/home/home.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {CropsManagementComponent} from "./crops/pages/crops-management/crops-management.component";
 import {CropsHistoryComponent} from "./crops/pages/crops-history/crops-history.component";
-import {CropsInformationComponent} from "./crops/pages/crops-information/crops-information.component";
+import {CropInformationComponent} from "./crops/pages/crops-information/crops-information.component";
 import {CropsStatisticsComponent} from "./crops/pages/crops-statistics/crops-statistics.component";
 import {ForumManagementComponent} from "./forum/pages/forum-management/forum-management.component";
 
@@ -11,7 +11,7 @@ export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path:'mycrops', component: CropsManagementComponent},
     {path:'history', component: CropsHistoryComponent},
-    {path:'info', component: CropsInformationComponent},
+    {path:'info', component: CropInformationComponent},
     {path:'statistics', component: CropsStatisticsComponent},
     {path:'forum',component:ForumManagementComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
--- src/app/crops/pages/crops-information/crops-information.component.ts
+++ src/app/crops/pages/crops-information/crops-information.component.ts
@@ -1,41 +1,24 @@
 import { Component } from '@angular/core';
-
 import {MatTabsModule} from '@angular/material/tabs';
+import {CropsControlsComponent} from "../../components/crop-controls/crop-controls.component";
+import {CropCaresComponent} from "../../components/crop-cares/crop-cares.component";
+import {
+  ProductsCreateAndEditComponent
+} from "../../components/crop-products-create-and-edit/crop-products-create-and-edit.component";
+import {CropDiseasesComponent} from "../../components/crop-diseases/crop-diseases.component";
+import {
+  CropGeneralInformationComponent
+} from "../../components/crop-general-information/crop-general-information.component";
+import {CropsProductsComponent} from "../../components/crop-products/crop-products.component";
 
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-import {Care} from "../../model/care.entity";
-import {SuggestionsService} from "../../services/suggestions.service";
 
 @Component({
-  selector: 'app-crops-information',
+  selector: 'app-crop-information',
   standalone: true,
-  imports: [MatTabsModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatTableModule],
-  providers: [provideNativeDateAdapter()],
+  imports: [MatTabsModule, CropsControlsComponent, CropCaresComponent, ProductsCreateAndEditComponent, CropDiseasesComponent, CropGeneralInformationComponent, CropsProductsComponent],
   templateUrl: './crops-information.component.html',
   styleUrl: './crops-information.component.css'
 })
-export class CropsInformationComponent {
+export class CropInformationComponent {
 
-  selected: Date | null;
-
-  care: Array<Care> = [];
-    displayedColumns: string[] = ['date', 'suggestion'];
-    dataSource: any;
-
-  constructor(private suggestionsService: SuggestionsService) {
-  this.selected = null;
-  }
-  ngOnInit(): void {
-    this.suggestionsService.getAll().subscribe((data: any) => {
-      this.care = data;
-      this.dataSource = new MatTableDataSource(this.care);
-    });
-    }
 }
