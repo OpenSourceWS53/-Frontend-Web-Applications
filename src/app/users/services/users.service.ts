0bab4b1295cf42844ab2b5695c41634c4f442053
PaoloGuilen <u202124343@upc.edu.pe>
2024-05-29 01:06:56 -0500

    feat: added page not found and services

--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {HttpClient} from "@angular/common/http";
+import {Answer} from "../model/answer.entity";
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
-
-  constructor() { }
-}
+export class UsersService extends BaseService<Answer> {
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/users/users';
+    }
+  }
